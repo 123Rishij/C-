f74a554998e9e0a5542b0ebcc400fda7548efd61
123Rishij <107739908+123Rishij@users.noreply.github.com>
2023-03-10 19:06:39 +0530

    Create multiplication table reverse.cpp

---
+++ multiplication table reverse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int x,y,z=10;
+ cout<<"enter a number";
+ cin>>x;
+
+ while(z>=1)
+ {
+ y=x*z;
+
+ cout<<x<<"*"<<z<<"="<<y<<endl;
+ z--;
+ }
+ }
