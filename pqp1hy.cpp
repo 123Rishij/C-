dff47cc21c7fdb48dca3868683286088246c7d09
123Rishij <107739908+123Rishij@users.noreply.github.com>
2023-03-10 19:21:35 +0530

    Create pqp1hy.cpp

---
+++ pqp1hy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string a = "John smith";
+    int b = 12;
+    int c = 31;
+    int d = 31;
+    cout<<"my name is"<< a<<endl;
+    cout<<"My age is"<< b<<endl;
+    cout<<"There are"<< c<<"students in my class"<<endl;
+    cout<<"There are"<< d<<"computers in my computer lab"<<endl;
+    return 0;
+
+
+}
