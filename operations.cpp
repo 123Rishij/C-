7cae64da93997ed7c54bd69a787b017160827330
123Rishij <107739908+123Rishij@users.noreply.github.com>
2023-03-10 19:20:36 +0530

    Create operations.cpp

---
+++ operations.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the two numbers a and b\n";
+    cin>>a>>b;
+    cout<<"a+b is :"<<a+b<<endl;
+    cout<<"a-b is :"<<a-b<<endl;
+    cout<<"a*b is :"<<a*b<<endl;
+    cout<<"a/b is :"<<a/b<<endl;
+    cout<<"a%b is :"<<a%b<<endl;
+    cout<<"post Increment value of a is :"<<a++<<endl;
+    cout<<"post decrement value of a is :"<<b--<<endl;
+
+}
