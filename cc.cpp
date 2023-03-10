7df6f92aacaede868da64647a5663b77ae972c5c
123Rishij <107739908+123Rishij@users.noreply.github.com>
2023-03-10 19:16:53 +0530

    Create cc.cpp

---
+++ cc.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+char p;
+
+cout<<"enter a value";
+cin>>p;
+
+if((p>= 'a' && p <= 'z')||(p>='A'&&a<='Z'))
+         {
+          cout<<a <<"is a alphabet";
+         }
+else if(p>='0'&&a<='10')
+         {
+          cout<<p<<"is a digit";
+         }
+else
+         {
+          cout<<p<<"is a special character";
+         }
+return 0;
+}
