4ba2ed51ce9999b633edb9c739cf91e2b980bc53
123Rishij <107739908+123Rishij@users.noreply.github.com>
2023-03-10 19:06:01 +0530

    Create multiplication table.cpp

---
+++ multiplication table.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int x,y,z=1;
+ cout<<"enter a number";
+ cin>>x;
+
+ while(z<=10)
+ {
+ y=x*z;
+
+ cout<<x<<"*"<<z<<"="<<y<<endl;
+ z++;
+ }
+ }
